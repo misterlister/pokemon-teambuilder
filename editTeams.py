ef21c1e8769fb917d66517942e7cc533a265a891
misterlister <haydenglister@gmail.com>
2024-05-23 14:14:58 -0700

    Added "get_player_name" and "choose_team_name" functions. Added formatting to print_list function.

--- editTeams.py
+++ editTeams.py
@@ -1,36 +1,85 @@
-import requests
-from constants import BASEURL
-import sqlite3
+from dbInteraction import (
+    get_player_names, 
+    get_team_names_from_version
+    )
+from apiInteraction import (
+    get_all_version_names
+)
 
-def create_team():
+def create_team(connection):
+    player_name = get_player_name(connection)
+    if player_name is None: return
     version = choose_version()
-    if version is None:
-        return
+    if version is None: return
+    teamName = choose_team_name(connection, version)
+    if teamName is None: return
+    
+def get_player_name(connection):
+    valid = False
+    existing_names = get_player_names(connection)
+    while not valid:
+        team_name = input("Please enter the name of the player who will use this team (eg. 'Ash'). Type 'list' to see all existing names: ")
+        if team_name == "list":
+                print_list(existing_names)
+        elif team_name in existing_names:
+            print("Sorry. That team name already exists for this game.")
+            if not confirm("Would you like to try again?"):
+                return None
+        else:
+            if confirm(f"You entered the name: '{team_name}'. Is this correct?"):
+                print("")
+                return team_name
     
 def choose_version():
     valid = False
     versions = get_all_version_names()
     versions.append("other")
     while not valid:
-        print_list(versions)
-        version = input("Please enter the game version for this team (eg. red): ")
+        version = input("Please enter the game version for this team (eg. red). Type 'list' to see all options: ")
         version = version.lower()
         if version not in versions:
-            print("Sorry. That is not a valid Pokemon game version.")
-            if not confirm("Would you like to try again?"):
-                return
+            if version == "list":
+                print_list(versions)
+            else:
+                print("Sorry. That is not a valid Pokemon game version.")
+                if not confirm("Would you like to try again?"):
+                    return None
         else:
-            print(f"Creating a new team for Pokemon {version} version!")
-            valid = True
+            if confirm(f"You selected: '{version}' version. Is this correct?"):
+                print("")
+                return version
             
 def print_list(list):
+    max_size = 1
+    if len(list) == 0:
+        print("No data exists")
+        return
+    for item in list:
+        if len(item) > max_size:
+            max_size = len(item) + 1
+    items_per_line = int(90 / max_size)
     for i in range(len(list)):
-        if i % 5 is 0:
+        if i % items_per_line == 0:
             print("")
-        print(f"{list[i]} ", end="")
+        print(f"{list[i].ljust(max_size, " ")} ", end="")
+    print("\n")
+    
+def choose_team_name(connection, version):
+    valid = False
+    existing_names = get_team_names_from_version(connection, version)
+    while not valid:
+        team_name = input("Please enter the name for this team (eg. 'ruby team'). Type 'list' to see all existing names: ")
+        if team_name == "list":
+                print_list(existing_names)
+        elif team_name in existing_names:
+            print("Sorry. That team name already exists for this game.")
+            if not confirm("Would you like to try again?"):
+                return None
+        else:
+            if confirm(f"You entered the name: '{team_name}'. Is this correct?"):
+                print("")
+                return team_name
         
-        
-
 def edit_team():
     pass
 
@@ -39,21 +88,12 @@ def confirm(question):
     while not valid:
         response = input(f"{question} Yes/No: ")
         response = response.lower()
-        if response == "yes":
+        if response == "yes" or response == 'y':
             return True
-        if response == "no":
+        if response == "no" or response == 'n':
             return False
         print("Invalid response, please try again.")
         
 
-def get_all_version_names():
-    url = f"{BASEURL}version"
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        data = response.json()
-        versions = data['results']
-        version_names = [version['name'] for version in versions]
-        return version_names
-    else:
-        print(f"Failed to fetch data: {response.status_code}")
+
+        
