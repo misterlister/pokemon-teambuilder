58bc38707eb6b948effa19f3e7da4cf271892636
misterlister <haydenglister@gmail.com>
2024-05-28 21:11:59 -0700

    Added clear functions to main

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from menus import main_menu
 from dbInteraction import init_database
-from printouts import (
-    greeting,
-    goodbye
-)
+from printouts import print_title
+from constants import GREETING, GOODBYE
+from textManip import press_enter, clear
 
 
 if __name__ == "__main__":
     connection = init_database()
-    greeting()
+    clear()
+    print_title(GREETING)
+    press_enter()
     main_menu(connection)
-    goodbye()
+    clear()
+    print_title(GOODBYE)
+    press_enter()
+    clear()
     connection.close()
