cc4f548ae251e8b1be5c66395e30f1fd07e38826
misterlister <haydenglister@gmail.com>
2024-05-28 21:12:29 -0700

    Adjusted printouts to be dynamically sized

--- printouts.py
+++ printouts.py
@@ -1,28 +1,14 @@
-from constants import (
-    LGBORDER,
-    MDBORDER,
-    SMBORDER
-)
+from constants import BORDER, WIDTH, INNER_WIDTH
 
-def greeting() -> None:
-    print(LGBORDER*2)
-    print("Welcome to the Pokemon Teambuilder!")
-    print(LGBORDER*2)
+def print_title(title: str) -> None:
+    print(BORDER * WIDTH)
+    print(title.center(WIDTH))
+    print(BORDER * WIDTH)
     
-def goodbye() -> None:
-    print(LGBORDER*3)
-    print("Thanks for using the Pokemon Teambuilder! See you later!")
-    print(LGBORDER*3)
-    
-def print_list(list: list, heading:str = None) -> None:
+def print_list(list: list, heading: str = None, border: bool = False) -> None:
     if heading:
-        if len(heading) < 20:
-            border = LGBORDER
-        elif len(heading) < 40:
-            border = MDBORDER
-        else:
-            border = SMBORDER
-        print("\n", border, heading, border)
+        print_heading(heading)
+    
     max_size = 1
     if len(list) == 0:
         print("No data exists\n")
@@ -32,15 +18,23 @@ def print_list(list: list, heading:str = None) -> None:
             if 4 > max_size: max_size = 5
         elif len(item) > max_size:
             max_size = len(item) + 1
-    items_per_line = int(90 / max_size)
+    items_per_line = int(INNER_WIDTH / max_size)
+    if border:
+        list_border = (max_size * min(items_per_line, len(list))) * BORDER
+        print("\t" + list_border)
+    print("\t", end="")
     for i in range(len(list)):
-        if i % items_per_line == 0 and i > 0:
-            print("")
+        if i % items_per_line == 0 and i != 0:
+            print("\n\t", end="")
         if list[i] == None:
             value = "None"
         else:
             value = list[i]
-        print(f"{value.ljust(max_size, " ")} ", end="")
-    print("")
+        print(f"{value.ljust(max_size, " ") }", end="")
+    if border:
+        print("\n\t" + list_border)
+    else:
+        print("")
     
-    
+def print_heading(heading: str) -> None:
+    print("\n" + heading.center(WIDTH, BORDER))
